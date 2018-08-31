e349d1ffc92ede90a1c72f8b32c1957a55cfde37
gracelizhuoqian <827924130@qq.com>
2018-08-31 12:53:53 +0800

    uploader

--- src/components/backEnd/EssayDetail.vue
+++ src/components/backEnd/EssayDetail.vue
@@ -20,6 +20,13 @@
               <el-input v-model="essay.image" style="width:710px" placeholder="请在此处输入图片链接"></el-input>
             </el-form-item>
           </el-col>
+          <el-col :span='20' :push="1">
+            <el-upload :action="actionUrl" list-type="picture" multiple :limit="10"
+                       :auto-upload="true" :on-success="uploaded"
+                       :data="postData" :before-upload="beforeUpload">
+              <el-button size="small" type="primary">点击上传图片</el-button>
+            </el-upload>
+          </el-col>
         </el-row>
         <el-row style="margin-top: 20px">
           <el-col :span='20' :push="1">
@@ -43,6 +50,7 @@
   import api from '../../api/index'
   import NProgress from 'nprogress'
   import {sub} from '../../assets/js/classifySubmit'
+  import moment from 'moment'
   export default {
     data() {
       return {
@@ -51,7 +59,7 @@
           title: '',
           classify: '',
           content: '',
-          image:'',
+          image: '',
         },
         classifyList: ["Draw", "Travel", "Fiction", "Delicious", "Others"],
         createRules: {
@@ -68,7 +76,14 @@
         load: false,
         btnText: "立即更新",
         listLoading: false,
-        isNew:true
+        isNew: true,
+        actionUrl:"http://upload-z0.qiniu.com",
+        fileList: [],
+        postData: {
+          token: 'Y_WZw7lla58snngtzqKceUGnA0c6b0Nc9zLexWGK:r5aNFj4WIEyKP9QbVgUu7Ht_XPE=:eyJzY29wZSI6Imxp' +
+          'emh1b3FpYW4xMjMiLCJkZWFkbGluZSI6MTUzOTk2NDUzM30='
+        },
+        outerUrl:'http://oy1usp3kb.bkt.clouddn.com/'
       }
     },
     methods: {
@@ -81,9 +96,9 @@
               .then(() => {
                 NProgress.start()
                 this.load = true;
-                // 换成真实API的时候可以直接提交this.article
+                // 换成真实API的时候可以直接提交
                 this.btnText = "更新中"
-                let essayAction=this.isNew?'createEssay':'updateEssay'
+                let essayAction = this.isNew ? 'createEssay' : 'updateEssay'
                 api[essayAction]({
                   ...this.essay
                 })
@@ -93,8 +108,8 @@
 
                     if (res.data) {
                       NProgress.done()
-                      sub(this,res)
-                      this.load=false
+                      sub(this, res)
+                      this.load = false
                     }
                   }).catch((err) => {
                   console.log(err);
@@ -112,10 +127,10 @@
         setTimeout(() => {
           NProgress.done();
           this.listLoading = false
-          if(this.$route.params.postId){
-            this.isNew=false;
+          if (this.$route.params.postId) {
+            this.isNew = false;
             api.getOneEssay({id: this.$route.params.postId})
-              .then(({data: {code, aEssay: {_id, classify, title, content,image}}}) => {
+              .then(({data: {code, aEssay: {_id, classify, title, content, image}}}) => {
                 if (code == 200) {
                   this.essay.id = _id
                   this.essay.classify = classify
@@ -131,9 +146,24 @@
       // 取消
       cancle() {
         this.$router.push({path: '/admin/essayList'});
-      }
+      },
+      uploaded(res,file,filelist){
+        let imgUrl=this.outerUrl+res.key;
+        this.essay.image=imgUrl;
+      },
+      beforeUpload(file) {    //在图片提交前进行验证
+        let curr = moment().format('YYYYMMDD').toString()
+        let prefix = moment(file.lastModified).format('HHmmss').toString()
+        let suffix = file.name
+        let key = encodeURI(`${curr}/${prefix}_${suffix}`)
+        this.postData.key=key;
+        const isLt2M = file.size / 1024 / 1024 < 2;
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+        }
+        return isLt2M;
+      },
     },
-    computed: {},
     mounted() {
       this.initial()
     }
